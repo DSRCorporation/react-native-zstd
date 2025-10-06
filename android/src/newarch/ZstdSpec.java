cad56291e007a6fbc3c18d46137412372b04523b
Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
2025-10-06 17:49:59 +0300

    Support React Native 0.73 (backport)

---
+++ android/src/newarch/ZstdSpec.java
@@ -0,0 +1,10 @@
+package com.reactnativezstd;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class ZstdSpec extends NativeZstdSpec {
+  ZstdSpec(ReactApplicationContext context) {
+    super(context);
+  }
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
-import type { Int32 } from 'react-native/Libraries/Types/CodegenTypes';
-import Zstd from './NativeZstd';
+import { NativeModules, Platform } from 'react-native';
 
-export function compress(
-  data: string,
-  compressLevel: number = 3
-): ReadonlyArray<Int32> {
+const LINKING_ERROR =
+  `The package 'react-native-zstd' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+// @ts-expect-error
+const isTurboModuleEnabled = global.__turboModuleProxy != null;
+
+const ZstdModule = isTurboModuleEnabled
+  ? require('./NativeZstd').default
+  : NativeModules.Zstd;
+
+const Zstd = ZstdModule
+  ? ZstdModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function compress(data: string, compressLevel: number = 3): Uint8Array {
   const out = Zstd.compress(data, compressLevel);
   return out;
 }
 
-export function decompress(data: ReadonlyArray<Int32>): string {
+export function decompress(data: Uint8Array): string {
   const out = Zstd.decompress(data);
   return out;
 }
